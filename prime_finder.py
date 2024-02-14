b5203040b908e05d615ff241fd6d0d5aec37076b
MacBook AIR <macbookair@Olamide-Francis.local>
2024-02-14 13:26:58 +0000

    prime finder

--- prime_finder.py
+++ prime_finder.py
@@ -1,1 +1,20 @@
-print("hello world")
+
+
+#determing if a number  is prime
+
+
+
+#function for determining prime
+def find_prime(number):
+    is_prime = True
+    for num in range(2, number):
+        if number % num ==0:
+            is_prime = False
+            break
+    if is_prime:
+        print("This is a prime number")
+    else:
+        print("This is not a prime number")
+
+number = int(input('Please enter a number: '))
+find_prime(number)
